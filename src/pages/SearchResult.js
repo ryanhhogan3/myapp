9cbb962428f805462ac3415b3dc6ca6554c96774
Mr. Ryan <60050719+ryanhhogan3@users.noreply.github.com>
2023-11-07 20:56:38 -0600

    new data, new search to flask api working

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from '../components/search'
 import { useParams } from 'react-router'
 
 export default function SearchResult() {
+  const [price, setPrice] = useState("");
   let {search} = useParams();
+
+  const fetchPrice = async (search) => {
+    try{
+      const response = await fetch(`https://flask-api-finlabs-b778fe863ba1.herokuapp.com/Stock/${search}/currentPrice`);
+      const data = await response.json();
+      console.log(data);
+      
+      setPrice(data);
+
+    }
+    catch(e){
+      console.log(e)
+      setPrice(0);
+    }
+  }
+
+  fetchPrice(search)
 
 
 
@@ -19,6 +37,8 @@ export default function SearchResult() {
       </h1>
       <h2>
         {search}
+        <br></br>
+        {price}
       </h2>
 
 
